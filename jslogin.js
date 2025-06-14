8d65c5790f2f76611597496a31fbc9dc6ae2634e
unknown <trongpvn2211@gmail.com>
2025-06-14 23:27:34 +0700

    update

--- jslogin.js
+++ jslogin.js
@@ -1,12 +1,19 @@
-document.getElementById("login-form").addEventListener("submit", function (e) {
+async function hashSHA256(text) {
+    const msgUint8 = new TextEncoder().encode(text);
+    const hb = await crypto.subtle.digest('SHA-256', msgUint8);
+    const ha = Array.from(new Uint8Array(hb));
+    return ha.map(b => b.toString(16).padStart(2, '0')).join('');
+  }
+
+  const ch = "9bf72d8a19d77b13fa3dbe67726d5ef7967b9c46038103ed528956b18ae18001";
+
+  document.getElementById("login-form").addEventListener("submit", async function (e) {
     e.preventDefault();
     const pass = document.getElementById("pass").value.trim();
-    const correctPass = "matkhau123"; // 👉 Mật khẩu đúng được định sẵn
+    const inputHash = await hashSHA256(pass);
 
-    if (pass === correctPass) {
+    if (inputHash === ch) {
       document.getElementById("login-form").classList.add("hidden");
       document.getElementById("news-board").classList.remove("hidden");
-    } else {
-      alert("Sai mật khẩu!");
     }
   });
